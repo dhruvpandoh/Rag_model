edd7615444890f9a05eea08e87317de8e2a59807
Dhruv Pandoh <93575057+dhruvpandoh@users.noreply.github.com>
2024-12-14 13:30:03 -0500

    Update deploy.py

--- rag_model/deploy.py
+++ rag_model/deploy.py
@@ -1,8 +1,8 @@
 from qdrant_client import QdrantClient
 from featurization_pipeline import featurize_text
 
-QDRANT_URI = "https://fc94b6ab-f5e6-4b45-8e7c-51ed48367a37.us-east4-0.gcp.cloud.qdrant.io:6333"
-QDRANT_API_KEY = "r7tCBpWwRoewwOOIP-VuZgRoka_YZGvwY24SzEK1K85CyU1-Ii772Q"
+QDRANT_URI = ""
+QDRANT_API_KEY = ""
 qdrant_client = QdrantClient(url=QDRANT_URI, api_key=QDRANT_API_KEY)
 
 def query_rag(question):
